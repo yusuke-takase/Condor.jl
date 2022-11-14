249881fc7ca6077808f60a1709c42111b21a83d6
okayamayu8 <49719415+okayamayu8@users.noreply.github.com>
2022-11-14 11:49:27 +0900

    Update Condor.jl

--- src/Condor.jl
+++ src/Condor.jl
@@ -10,15 +10,16 @@ using LinearAlgebra
 using ProgressMeter
 using HDF5
 using Interpolations
+using Falcons
 #const hp = pyimport("healpy")
 #np = pyimport("numpy")
 #pd = pyimport("pandas")
 using WignerD
-
+#=
 function __init__()
     copy!(hp, pyimport_conda("healpy", "healpy"))
 end
-
+=#
 include("./function/beam_func.jl")
 include("./function/convolution_func.jl")
 include("./function/adding_YN_func.jl")
