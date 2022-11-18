828405f17c982820cb7e812eac5c03437d5f17a5
Wang Wang <wangwbnu@outlook.com>
2022-11-18 18:34:14 +0900

    Update beam_alm.jl

--- src/beam/beam_alm.jl
+++ src/beam/beam_alm.jl
@@ -32,4 +32,15 @@ end
 
 function bm_alm_free(alm)
     alm = nothing
-end bm_alm_free(alm)
+end
+
+function truncate_alm(alm, lmax, mmax)
+    alm_new = Alm(lmax,mmax)
+    size = numberOfAlms(lmax, min(mmax,alm.mmax))
+    for idx in 1:size
+        m = convert(Int64, ceil(((2 * lmax + 1) - sqrt((2 * lmax + 1) ^ 2 - 8 * (idx - 1 - lmax))) / 2))
+        l = idx - 1 - m * div(2 * lmax + 1 - m,2)
+        alm_new.alm[idx] = alm.alm[almIndex(alm, l, m)]
+    end
+    return alm_new
+end
