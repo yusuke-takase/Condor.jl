58c466d2af22b582fb03a1c711b0db1efdadb17b
okayamayu8 <49719415+okayamayu8@users.noreply.github.com>
2023-02-21 12:20:52 +0100

    Update Condor.jl

--- src/Condor.jl
+++ src/Condor.jl
@@ -22,12 +22,15 @@ function __init__()
     copy!(hp, pyimport_conda("healpy", "healpy"))
 end
 =#
-include("./function/beam_func.jl")
-include("./function/convolution_func.jl")
+
 include("./function/adding_YN_func.jl")
+include("./function/tools.jl")
+include("./function/convolution.jl")
+include("./function/scanning.jl")
+
 
 export get_2Ddata, symmetrizer, dBi, gen_beammap, beam_pointor, AlmPair, gen_Blm, gen_GaussBeammap
 export EffectiveBeamConvolution, GaussBeamConvolution, convolvor
 export gauss_3d_xyz, make_beam_TQU, unique_theta, FFTConv_demo, get_psi_make_TOD, solver_matrix, for_healpy_order, pix_calcmax, FFTConv_demo_onlyT, get_psi_make_TOD_T, unique_theta_detect
-
+export FFTConvolution_QU, FFTConvolution_T, get_psi_make_TOD_TQU_HWP, solver_matrix_TQU
 end
