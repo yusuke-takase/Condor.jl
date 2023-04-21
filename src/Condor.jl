0fd3904b3eda4e918a13f718e3c1804cd3f1e0c6
YuyaNagano <naganoyuukana@naganoyuunarinoMacBook-Air.local>
2023-04-21 12:40:05 +0900

    addconvolution

--- src/Condor.jl
+++ src/Condor.jl
@@ -17,6 +17,7 @@ using Falcons
 using WignerD
 using FFTW
 using NPZ
+
 #=
 function __init__()
     copy!(hp, pyimport_conda("healpy", "healpy"))
@@ -27,10 +28,12 @@ end
 include("./function/tools.jl")
 include("./function/convolution.jl")
 include("./function/scanning.jl")
+include("./function/structure_define.jl")
 
 
 export get_2Ddata, symmetrizer, dBi, gen_beammap, beam_pointor, AlmPair, gen_Blm, gen_GaussBeammap
 export EffectiveBeamConvolution, GaussBeamConvolution, convolvor
 export gauss_3d_xyz, make_beam_TQU, unique_theta, FFTConv_demo, get_psi_make_TOD, solver_matrix, for_healpy_order, pix_calcmax, FFTConv_demo_onlyT, get_psi_make_TOD_T, unique_theta_detect
 export FFTConvolution_QU, FFTConvolution_T, get_psi_make_TOD_TQU_HWP, solver_matrix_TQU
+export get_pointings_theta_phi_psi_alpha_pix_tod, tod_convolution_idalhwp
 end
